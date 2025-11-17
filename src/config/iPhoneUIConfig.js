7de52c0711aebfe1649429236f3cafe10a4b79ee
thond02 <thond02@ominext.com>
2025-11-17 23:28:40 +0700

    iphne bottom

--- src/config/iPhoneUIConfig.js
+++ src/config/iPhoneUIConfig.js
@@ -14,7 +14,7 @@ export const iPhoneUIConfig = {
 
   // Safe area insets cho LANDSCAPE (đọc từ CSS env variables + fallback)
   getSafeAreaInsets: () => {
-    let top = 0, right = 0, bottom = 10, left = 0;
+    let top = 0, right = 0, bottom = 5, left = 0;
     return { top, right, bottom, left };
   },
 
